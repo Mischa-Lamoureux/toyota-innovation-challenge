1ea2a6cd4786fc3a8bcdffa6459003b388a17d77
Kael <kmurph163@gmail.com>
2023-05-13 14:37:58 -0400

    Overlays the contours of two images

--- challenge-1/challenge-1.py
+++ challenge-1/challenge-1.py
@@ -1,1 +1,38 @@
-print("Challenge 1")
+import cv2
+import numpy as np
+
+userinp = input('What image do you want to look at: ')
+
+userimage = cv2.imread('assets/Metal/{}'.format(userinp))
+image = cv2.imread('assets\Metal\Metal_1.jpg')
+
+def getcontour(image, userimage):
+    basecontour = contour(image)
+    usercontour = contour(userimage)
+    
+    newimage = overlaycontours(basecontour, userimage, (0,255,0))
+    finalimage = overlaycontours(usercontour, newimage, (0,0,255))
+
+    return finalimage
+
+def contour(image):
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    _, binary = cv2.threshold(gray, 25, 255, cv2.THRESH_BINARY_INV)
+
+    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+def overlaycontours(contours, userimage, color):
+    for contour in contours:
+        
+        # BGR (blue, green, red)
+        # green (uncovered)
+        cv2.drawContours(userimage, [contour], -1, color, 2)
+    return userimage
+
+finalimage = getcontour(image, userimage)
+
+cv2.imshow('contoured', finalimage)
+cv2.waitKey(0)
